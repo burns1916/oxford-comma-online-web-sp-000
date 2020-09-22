88937d77e336d1582403e967cb95c65dd5fc30f9
Ryan K Burns <ryankburns@gmail.com>
2020-09-22 18:24:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
 case array
+when array.length > 2
+  array[-1].prepend "and "
+  array.join(", ")
 when array.length == 2
-  array.join("and")
+  array[-2] << " and"
+  array.join
 else
   array.join
 end
